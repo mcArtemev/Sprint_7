7c51361e0a81a0e11ad5003a8313e0f80ee20037
mcArtemev <aluygospodin@yandex.ru>
2023-01-21 23:31:45 +0300

    Optimized Parametrized tests

--- src/test/java/Order/CreateNewOrderTest.java
+++ src/test/java/Order/CreateNewOrderTest.java
@@ -17,45 +17,19 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 
 @RunWith(Parameterized.class)
 public class CreateNewOrderTest {
-    private final String firstName;
-    private final String lastName;
-    private final String address;
-    private final String metroStation;
-    private final String phone;
-    private final int rentTime;
-    private final String deliveryDate;
-    private final String comment;
-    private final String[] color;
-
+    private final Object order;
     TestFixture testFixture = new TestFixture();
 
-    public CreateNewOrderTest(
-            String firstName,
-            String lastName,
-            String address,
-            String metroStation,
-            String phone,
-            int rentTime,
-            String deliveryDate,
-            String comment,
-            String[] color) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.metroStation = metroStation;
-        this.phone = phone;
-        this.rentTime = rentTime;
-        this.deliveryDate = deliveryDate;
-        this.comment = comment;
-        this.color = color;
+    public CreateNewOrderTest(Object order) {
+        this.order = order;
     }
     @Parameterized.Parameters
     public static Object[][] getOrderData() {
         return new Object[][] {
-                { "testName","testLastname","Test street 1","MetroTest","89999999999",7,"2022-01-01","Test comment", new String[]{"BLACK"}},
-                { "testName","testLastname","Test street 1","MetroTest","89999999999",7,"2022-01-01","Test comment", new String[]{"GREY"}},
-                { "testName","testLastname","Test street 1","MetroTest","89999999999",7,"2022-01-01","Test comment", new String[]{"BLACK", "GREY"}},
-                { "testName","testLastname","Test street 1","MetroTest","89999999999",7,"2022-01-01","Test comment", new String[]{}},
+                {new CreateOrderSerializer("testName","testLastname","Test street 1","MetroTest","89999999999",7,"2022-01-01","Test comment", new String[]{"BLACK"})},
+                {new CreateOrderSerializer( "testName","testLastname","Test street 1","MetroTest","89999999999",7,"2022-01-01","Test comment", new String[]{"GREY"})},
+                {new CreateOrderSerializer( "testName","testLastname","Test street 1","MetroTest","89999999999",7,"2022-01-01","Test comment", new String[]{"BLACK", "GREY"})},
+                {new CreateOrderSerializer( "testName","testLastname","Test street 1","MetroTest","89999999999",7,"2022-01-01","Test comment", new String[]{})},
         };
     }
         @Before
@@ -67,19 +41,9 @@ public class CreateNewOrderTest {
         @DisplayName("Try to create new order with variable color")
         @Description("Test /api/v1/orders with variable colors. Return 201 and track")
         public void createOrderWithVariableColorExpectedStatus201() {
-        CreateOrderSerializer orderDataJson = new CreateOrderSerializer(
-                firstName,
-                lastName,
-                address,
-                metroStation,
-                phone,
-                rentTime,
-                deliveryDate,
-                comment,
-                color);
             Response response = given()
                     .header("Content-type", "application/json")
-                    .body(orderDataJson)
+                    .body(order)
                     .when()
                     .post("/api/v1/orders");
             response.then().assertThat().statusCode(201);
